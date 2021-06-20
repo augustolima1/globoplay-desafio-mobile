79788f989ffbb346c2ca2dac7ff085dfda103619
augustolima1 <62573696+augustolima1@users.noreply.github.com>
2021-06-20 18:42:14 -0300

    --

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
@@ -6,17 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  @ViewChild("header") header: HTMLElement;
 
   slidesOptions = {
-    slidesPerView: 3.2
+    slidesPerView: 3.2,
+    spaceBetween: 5,
+    freeMode: true
   };
 
-  slidesOptionsTop = {
-    slidesPerView: 2
-  };
+ 
+  
 
+  constructor(
+    public element: ElementRef, 
+    public renderer: Renderer2
+   ) {
+  }
+ 
+  ionViewWillEnter() {
+    this.renderer.setStyle(this.header['el'], 'webkitTransition', 'top 700ms');
+  }
 
-  constructor() {}
+  onContentScroll(event) {
+    if (event.detail.scrollTop >= 50) {
+      this.renderer.setStyle(this.header['el'], 'top', '-16px');
+    } else {
+      this.renderer.setStyle(this.header['el'], 'top', '0px');
+    }
+  }
 
 }
